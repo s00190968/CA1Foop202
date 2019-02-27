90da4c2a769839f4cc5a996a3ab03000442ae65d
Janessa Aulén <S00190968@mail.itsligo.ie>
2019-02-27 15:32:06 +0000

    Classes for taks made.

---
+++ TaskManagementApp/RealTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApp
+{
+    class RealTask:Task
+    {
+        public enum PRIORITY_TYPES { Low, Normal, Medium, Urgent, SuperUrgent, HyperUrgent }
+        public PRIORITY_TYPES Priority { get; }
+        DateTime DueDate { get; set; }
+
+        public RealTask(string title, string description, CATEGORY category, PRIORITY_TYPES priority, DateTime dueDate) : base(title, description, category)
+        {
+            Priority = priority;
+            DueDate = dueDate;
+        }
+    }
+}
---
+++ TaskManagementApp/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApp
+{
+    class Task
+    {
+        public enum CATEGORY { Home, Work, School, Leisure };
+        public CATEGORY taskCategory;
+        //title, description, category
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Task(string title, string description, CATEGORY category)
+        {
+            Title = title;
+            Description = description;
+            taskCategory = category;
+        }
+    }
+}
